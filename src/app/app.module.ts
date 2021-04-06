f2529bab94defc6c3b01c6258e70a31bedb4a83e
BrunoSampaioCesario <bruno.cesario@sabion.com.br>
2021-04-06 11:33:08 -0300

    importação da tela de login e lista de clientes e modulo de formulario e do AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormularioClienteComponent } from './formulario-cliente/formulario-cliente.component';
 import { TelaLoginComponent } from './tela-login/tela-login.component';
+import { ListaClientesComponent } from './lista-clientes/lista-clientes.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormularioClienteComponent,
-    TelaLoginComponent
+    TelaLoginComponent,
+    ListaClientesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
