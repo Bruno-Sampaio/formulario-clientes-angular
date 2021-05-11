69a1c53e0efe131f9b91bccd1a8870e450e8c64b
BrunoSampaioCesario <bruno.cesario@sabion.com.br>
2021-05-11 12:43:11 -0300

    importação do modulo de rotas e do formsModule e reactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormularioClienteComponent } from './formulario-cliente/formulario-cliente.component';
 import { TelaLoginComponent } from './tela-login/tela-login.component';
-import { ListaClientesComponent } from './lista-clientes/lista-clientes.component';
-import { FormsModule } from '@angular/forms';
+import { ListaClienteComponent } from './lista-clientes/lista-clientes.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -13,11 +13,13 @@ import { AppRoutingModule } from './app-routing.module';
     AppComponent,
     FormularioClienteComponent,
     TelaLoginComponent,
-    ListaClientesComponent
+    ListaClienteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
